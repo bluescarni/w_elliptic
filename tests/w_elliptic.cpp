6f27f32d907f031dcbaf28092f063ea43bfd2fd9
Francesco Biscani <bluescarni@gmail.com>
2015-04-08 15:58:29 +0200

    Add some tests for zeta.

--- tests/w_elliptic.cpp
+++ tests/w_elliptic.cpp
@@ -103,6 +103,7 @@ static std::vector<std::vector<std::string>> test_02_data = read_data("../tests/
 static std::vector<std::vector<std::string>> test_03_data = read_data("../tests/test_03_data.txt");
 static std::vector<std::vector<std::string>> test_04_data = read_data("../tests/test_04_data.txt");
 static std::vector<std::vector<std::string>> test_05_data = read_data("../tests/test_05_data.txt");
+static std::vector<std::vector<std::string>> test_06_data = read_data("../tests/test_06_data.txt");
 
 typedef boost::mpl::vector<float,double,long double> real_types;
 
@@ -263,7 +264,7 @@ struct tester_03
                 acc_P_err += std::abs((P-P_comp)/P_comp);
             }
         }
-        std::cout << "\tMax P error: " << max_P_err << " @ [g2=" << test_03_data[max_err_idx][0u] << ",g3=" << test_03_data[max_err_idx][1u] << ",x=" << max_err_x << ",P=" << max_err_P << "]\n";
+        std::cout << "\tMax P error: " << max_P_err << " @ [g2=" << test_03_data[max_err_idx][0u] << ",g3=" << test_03_data[max_err_idx][1u] << ",c=" << max_err_x << ",P=" << max_err_P << "]\n";
         std::cout << "\tAverage P error: " << acc_P_err / (real_type(test_03_data.size())*100) << '\n';
     }
 };
@@ -351,7 +352,7 @@ struct tester_05
                 acc_P_err += std::abs((P-P_comp)/P_comp);
             }
         }
-        std::cout << "\tMax P' error: " << max_P_err << " @ [g2=" << test_05_data[max_err_idx][0u] << ",g3=" << test_05_data[max_err_idx][1u] << ",x=" << max_err_x << ",P=" << max_err_P << "]\n";
+        std::cout << "\tMax P' error: " << max_P_err << " @ [g2=" << test_05_data[max_err_idx][0u] << ",g3=" << test_05_data[max_err_idx][1u] << ",c=" << max_err_x << ",P'=" << max_err_P << "]\n";
         std::cout << "\tAverage P' error: " << acc_P_err / (real_type(test_05_data.size())*100) << '\n';
     }
 };
@@ -361,5 +362,48 @@ BOOST_AUTO_TEST_CASE(test_05)
     std::cout << "Testing the computation of complex P'\n";
     std::cout << "=====================================\n";
     boost::mpl::for_each<real_types>(tester_05());
+    std::cout << "\n\n\n";
+}
+
+struct tester_06
+{
+    template <typename RealType>
+    void operator()(const RealType &)
+    {
+        std::cout << "Testing type: " << typeid(RealType).name() << '\n';
+        using real_type = RealType;
+        real_type g2, g3, x, P, P_comp, max_P_err = 0, acc_P_err = 0;
+        size_type max_err_idx = 0;
+        std::string max_err_x, max_err_P;
+        for (size_type i = 0u; i < test_06_data.size(); ++i) {
+            const auto &v = test_06_data[i];
+            // Read the invariant values from mpmath.
+            real_from_str(g2,v[0]);
+            real_from_str(g3,v[1]);
+            // Build the W object.
+            we<real_type> w(g2,g3);
+            for (decltype(v.size()) j = 2u; j < v.size(); j += 2u) {
+                real_from_str(x,v[j]);
+                P = w.zeta(x);
+                real_from_str(P_comp,v[j + 1u]);
+                if (std::abs((P-P_comp)/P_comp) > max_P_err) {
+                    max_P_err = std::abs((P-P_comp)/P_comp);
+                    max_err_idx = i;
+                    max_err_x = v[j];
+                    max_err_P = v[j+1];
+                }
+                acc_P_err += std::abs((P-P_comp)/P_comp);
+            }
+        }
+        std::cout << "\tMax zeta error: " << max_P_err << " @ [g2=" << test_06_data[max_err_idx][0u] << ",g3=" << test_06_data[max_err_idx][1u] << ",x=" << max_err_x << ",zeta=" << max_err_P << "]\n";
+        std::cout << "\tAverage zeta error: " << acc_P_err / (real_type(test_06_data.size())*100) << '\n';
+    }
+};
+
+BOOST_AUTO_TEST_CASE(test_06)
+{
+    std::cout << "Testing the computation of real zeta\n";
+    std::cout << "====================================\n";
+    boost::mpl::for_each<real_types>(tester_06());
     std::cout << "\n\n\n";
 }
