019cc52d1987eeef3130e5146ea10f8d8665950c
Francesco Biscani <bluescarni@gmail.com>
2015-10-22 16:51:31 +0200

    Remove the source code.

--- cmake_modules/gcc_check_version.cpp
+++
@@ -1,8 +0,0 @@
-#if __GNUC__  < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 7)
-        #error Minimum GCC version supported is 4.7.0.
-#endif
-
-int main()
-{
-    return 0;
-}
--- cmake_modules/intel_check_version.cpp
+++
@@ -1,8 +0,0 @@
-#if __INTEL_COMPILER < 1500
-        #error Minimum Intel compiler version supported is 15.
-#endif
-
-int main()
-{
-    return 0;
-}
